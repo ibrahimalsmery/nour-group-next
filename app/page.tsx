81ebba6bdf8b2f52ae3cee3d4635a7d5e7b540ef
ibrahim alsmery <54236016+ibrahimalsmery@users.noreply.github.com>
2025-09-09 13:37:21 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div >
-      <h1>Here noor group</h1>
+      <h1>Here noor group 2025</h1>
     </div>
   );
 }
